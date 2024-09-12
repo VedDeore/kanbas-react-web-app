b8c2949dd33dd1deacf039495b2ce949b0818b40
Ved Deore <veddeore2312@gmail.com>
2024-09-12 17:26:35 -0400

    Commented logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import Labs from "./Labs";
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";

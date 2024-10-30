285c8cbe8a4193cf9462613154509c474ad19475
Ved Deore <veddeore2312@gmail.com>
2024-10-30 11:58:47 -0400

    Hide extra buttons except faculty

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -13,33 +13,36 @@ export default function CourseStatus() {
     <div id="wd-course-status" style={{ width: "300px" }}>
       <h2>Course Status</h2>
       {currentUser.role === "FACULTY" && (
-        <div className="d-flex">
-          <div className="w-50 pe-1">
-            <button className="btn btn-lg btn-secondary w-100 text-nowrap ">
-              <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish
-            </button>
+        <>
+          <div className="d-flex">
+            <div className="w-50 pe-1">
+              <button className="btn btn-lg btn-secondary w-100 text-nowrap ">
+                <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish
+              </button>
+            </div>
+            <div className="w-50">
+              <button className="btn btn-lg btn-success w-100">
+                <FaCheckCircle className="me-2 fs-5" /> Publish
+              </button>
+            </div>
           </div>
-          <div className="w-50">
-            <button className="btn btn-lg btn-success w-100">
-              <FaCheckCircle className="me-2 fs-5" /> Publish
-            </button>
-          </div>
-        </div>
+
+          <br />
+          <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+            <BiImport className="me-2 fs-5" />
+            Import Existing Content
+          </button>
+          <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+            <LiaFileImportSolid className="me-2 fs-5" />
+            Import from Commons
+          </button>
+          <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+            <IoMdHome className="me-2 fs-5" />
+            Choose Home Page
+          </button>
+          <br />
+        </>
       )}
-      <br />
-      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <BiImport className="me-2 fs-5" />
-        Import Existing Content
-      </button>
-      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <LiaFileImportSolid className="me-2 fs-5" />
-        Import from Commons
-      </button>
-      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <IoMdHome className="me-2 fs-5" />
-        Choose Home Page
-      </button>
-      <br />
       <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
         <RiBarChart2Fill className="me-2 fs-5" />
         View Course Screen
@@ -50,11 +53,16 @@ export default function CourseStatus() {
         View Announcement
       </button>
       <br />
-      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <RiBarChart2Fill className="me-2 fs-5" />
-        New Analytics
-      </button>
-      <br />
+      {currentUser.role === "FACULTY" && (
+        <>
+          <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+            <RiBarChart2Fill className="me-2 fs-5" />
+            New Analytics
+          </button>
+          <br />
+        </>
+      )}
+
       <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
         <IoIosNotifications className="me-2 fs-5" />
         View Course Notifications

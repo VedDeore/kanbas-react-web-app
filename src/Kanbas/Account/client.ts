34903e8111fdc9a7fdaad572b5233d368d883d2a
Ved Deore <veddeore2312@gmail.com>
2024-11-23 13:18:10 -0500

    Added remote database

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -3,6 +3,11 @@ export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 const axiosWithCredentials = axios.create({ withCredentials: true });
 
+export const findAllUsers = async () => {
+  const response = await axiosWithCredentials.get(USERS_API);
+  return response.data;
+};
+
 export const findMyCourses = async () => {
   const { data } = await axiosWithCredentials.get(
     `${USERS_API}/current/courses`
@@ -41,4 +46,24 @@ export const createCourse = async (course: any) => {
     course
   );
   return data;
+};
+export const findUsersByRole = async (role: string) => {
+  const response = await axiosWithCredentials.get(`${USERS_API}?role=${role}`);
+  return response.data;
+};
+export const findUsersByPartialName = async (name: string) => {
+  const response = await axiosWithCredentials.get(`${USERS_API}?name=${name}`);
+  return response.data;
+};
+export const findUserById = async (id: string) => {
+  const response = await axiosWithCredentials.get(`${USERS_API}/${id}`);
+  return response.data;
+};
+export const deleteUser = async (userId: string) => {
+  const response = await axios.delete(`${USERS_API}/${userId}`);
+  return response.data;
+};
+export const createUser = async (user: any) => {
+  const response = await axios.post(`${USERS_API}`, user);
+  return response.data;
 };
